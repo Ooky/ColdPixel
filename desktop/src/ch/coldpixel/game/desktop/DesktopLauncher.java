f97f232bebbddfe808916d7c92c0afa47062509b
Luca Dias <luca.dias@hotmail.ch>
2014-09-24 21:24:20 +0200

    Config Frame
    config: - widht ; height ; color ; Icon

--- desktop/src/ch/coldpixel/game/desktop/DesktopLauncher.java
+++ desktop/src/ch/coldpixel/game/desktop/DesktopLauncher.java
@@ -1,12 +1,21 @@
 package ch.coldpixel.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ch.coldpixel.game.Main;
+import com.badlogic.gdx.graphics.Color;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+                config.width = 600;
+                config.height = 500;
+                config.title = "Cold Pixel - Runner";
+                config.resizable = false;
+                
+                config.addIcon("Graphics/Icon/Icon.png", Files.FileType.Internal);
+                
+                new LwjglApplication(new Main(), config);
 	}
 }
