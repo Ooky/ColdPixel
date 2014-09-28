e037d76a382e80779835e42d75047e4f49c99c6f
Nelai <blki@hotmail.com>
2014-09-28 17:37:16 +0200

    FPS, Level_Size

--- core/src/ch/coldpixel/game/Main.java
+++ core/src/ch/coldpixel/game/Main.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
@@ -14,17 +13,19 @@ public class Main extends ApplicationAdapter {
 //==============================================================================
 //Initialization
 //==============================================================================
-    //Config
+    //DesktopLauncher
+    public static final boolean RESZIABLE = false;
+    public static final boolean MAX_FPS=false;
     public static final int WINDOW_WIDTH = 1024;
     public static final int WINDOW_HEIGTH = 512 + 128;
-    public static final int LEVEL_1_WIDTH = WINDOW_WIDTH*3;
-    public static final int LEVEL_1_HEIGTH = WINDOW_HEIGTH;
     public static final String GAMENAME = "Cold Pixel - Runner";
     public static final String FAVICON = "Graphics/Icon/Icon.png";
+    //LevelSize
+    public static final int LEVEL_1_WIDTH = WINDOW_WIDTH * 3;
+    public static final int LEVEL_1_HEIGTH = WINDOW_HEIGTH;
     private final int TILESIZE = 32;
     //Textures
     SpriteBatch batch;
-    Texture img;
     //FPS
     FPSLogger fps;
     //Array
@@ -40,7 +41,6 @@ public class Main extends ApplicationAdapter {
         //Spritebatch
         batch = new SpriteBatch();
         //Textures
-        img = new Texture("badlogic.jpg");
         //FPS
         fps = new FPSLogger();
         //Array
@@ -53,9 +53,6 @@ public class Main extends ApplicationAdapter {
     public void render() {
         Gdx.gl.glClearColor(0, 255, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
         showGrid();
         fps.log();
     }
@@ -63,7 +60,6 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
         grid.dispose();
     }
 
