e24e1d1d5bb32518c8b285d428fa4e7e17e17bf8
Nelai <blki@hotmail.com>
2014-09-28 17:07:29 +0200

    Del TilePic, Added ShowGrid

--- core/src/ch/coldpixel/game/Main.java
+++ core/src/ch/coldpixel/game/Main.java
@@ -2,6 +2,8 @@ package ch.coldpixel.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,19 +16,22 @@ public class Main extends ApplicationAdapter {
 //==============================================================================
     //Config
     public static final int WINDOW_WIDTH = 1024;
-    public static final int WINDOW_HEIGTH = 512+128;
+    public static final int WINDOW_HEIGTH = 512 + 128;
+    public static final int LEVEL_1_WIDTH = WINDOW_WIDTH*3;
+    public static final int LEVEL_1_HEIGTH = WINDOW_HEIGTH;
     public static final String GAMENAME = "Cold Pixel - Runner";
     public static final String FAVICON = "Graphics/Icon/Icon.png";
-    private final int TILESIZE = 16;
+    private final int TILESIZE = 32;
     //Textures
     SpriteBatch batch;
     Texture img;
-    Texture test;
     //FPS
     FPSLogger fps;
     //Array
     int kachelarr[][];
-    
+    //Shaperenderer
+    ShapeRenderer grid;
+
 //==============================================================================
 //Methods
 //==============================================================================
@@ -36,11 +41,12 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         //Textures
         img = new Texture("badlogic.jpg");
-        test = new Texture("Unbenannt.png");
         //FPS
         fps = new FPSLogger();
         //Array
-        kachelarr = new int[WINDOW_WIDTH/TILESIZE][WINDOW_WIDTH/TILESIZE];
+        kachelarr = new int[WINDOW_WIDTH / TILESIZE][WINDOW_WIDTH / TILESIZE];
+        //Shaperenderer
+        grid = new ShapeRenderer();
     }
 
     @Override
@@ -48,13 +54,9 @@ public class Main extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 255, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        for(int i = 0; i< WINDOW_WIDTH; i+=TILESIZE) {
-            for(int j = 0; j < WINDOW_HEIGTH; j+=TILESIZE) {
-                batch.draw(test, i, j);
-            }
-        }
         batch.draw(img, 0, 0);
         batch.end();
+        showGrid();
         fps.log();
     }
 
@@ -62,5 +64,21 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         img.dispose();
+        grid.dispose();
+    }
+
+    private void showGrid() {
+        //Show Grid if G is pressed
+        if (Gdx.input.isKeyPressed(Input.Keys.G)) {
+            grid.begin(ShapeRenderer.ShapeType.Line);
+            grid.setColor(Color.RED);
+            for (int i = 0; i <= LEVEL_1_WIDTH; i += TILESIZE) {
+                for (int j = 0; j <= LEVEL_1_HEIGTH; j += TILESIZE) {
+                    grid.rect(0, 0, i, j);
+                }
+            }
+        }
+        grid.setColor(Color.CLEAR);
+        grid.end();
     }
 }
